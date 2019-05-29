4848b3dd980fde7c6285b8f58b4604c6789303ff
thomasdecactus <thomaszijl@live.nl>
2019-05-29 14:42:38 +0200

    recourses fixed

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,19 +1,13 @@
 package application;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.SceneBuilder;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.awt.image.ImageObserver;
-import java.awt.image.ImageProducer;
 import java.io.IOException;
 
 public class Main extends Application {
