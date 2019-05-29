230ce98eed4b558311065369aaa95c7152af928c
thomasdecactus <thomaszijl@live.nl>
2019-05-29 12:53:20 +0200

    whoeps

---
+++ src/main/java/app/controllers/Main.java
@@ -0,0 +1,35 @@
+package app.controllers;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage){
+
+
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("../views/app.fxml"));
+            primaryStage.setTitle("yeet World");
+            primaryStage.setScene(new Scene(root, 300, 275));
+            primaryStage.show();
+
+        }
+        catch (Exception e){
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+}
