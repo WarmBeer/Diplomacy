7c267f6bca2c3be8017f2490a1c56d0c6cbf8c4a
thomasdecactus <thomaszijl@live.nl>
2019-06-03 10:53:16 +0200

    Chatbox, button/textfield error

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,19 +1,12 @@
 package application;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.SceneBuilder;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
-import java.io.IOException;
+import views.GameView;
 
 public class Main extends Application {
 
-        private static String GAME_VIEW = "/resources/GameView.fxml";
-        private static String STYLESHEET_FILE = "/resources/style.css";
+        GameView game;
 
         public static void main(String[] args) {
                 launch(args);
@@ -21,17 +14,7 @@ public class Main extends Application {
 
         @Override
         public void start(Stage stage) throws Exception {
-                initGui(stage);
+                game = new GameView(stage);
         }
 
-        private void initGui(Stage stage) throws IOException {
-                Parent root = FXMLLoader.load(getClass().getResource(GAME_VIEW));
-                Scene scene = SceneBuilder.create().root(root).width(500).height(530)
-                        .fill(Color.GRAY).build();
-                scene.getStylesheets().add(STYLESHEET_FILE);
-                stage.setScene(scene);
-                stage.setTitle("Diplomacy");
-                //stage.getIcons().add(ICON);
-                stage.show();
-        }
 }
--- src/views/GameView.java
+++ src/views/GameView.java
@@ -1,4 +1,73 @@
 package views;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.SceneBuilder;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+
 public class GameView {
+
+    private static String GAME_VIEW = "/resources/GameViewXML.fxml";
+    private static String STYLESHEET_FILE = "/resources/style.css";
+    private Parent content;
+
+    public GameView(Stage stage){
+        chatboxLaunch(stage);
+
+    }
+
+    public void chatboxLaunch(Stage primaryStage) {
+        String sceneFile = GAME_VIEW;
+
+        try{
+            URL url = getClass().getResource(sceneFile);
+            FXMLLoader loader = new FXMLLoader(url);
+            loader.setController(this);
+            content = loader.load();
+
+            primaryStage.setTitle("Welkom!");
+            primaryStage.setScene(new Scene(content, 400, 500));
+            primaryStage.show();
+        }
+        catch(IOException IOE){
+            IOE.printStackTrace();
+        }
+        catch(Exception E){
+            E.printStackTrace();
+        }
+
+    }
+
+    @FXML
+    private Pane TotaleChatbox;
+
+    @FXML
+    private TextArea berichtInput;
+
+    @FXML
+    private Button VerzendButton;
+
+    @FXML
+    private VBox VerticaleBox;
+
+    @FXML
+    private Scrollbar Scrollbarretje;
+
+//    @FXML
+//    private void verzendString(ActionEvent event) {
+//        System.out.println("Verzendbutton Gedrukt");
+//        //firebaseservice.addMessage(); --> Send messaage to firebase, than update the chat and get all messages from firebase
+//
+//    }
 }
+
