c48e875ce2646856ff074a3ff57acbdda63c82ab
WarmBeer <mickvd99@gmail.com>
2019-06-11 11:39:50 +0200

    Game and Player domain (Unfinished)

--- src/domains/Game.java
+++ src/domains/Game.java
@@ -1,8 +1,19 @@
 package domains;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Game {
+
+    private final String[] nations = {
+            "France",
+            "England",
+            "Austria-Hungary",
+            "Russia",
+            "Germany",
+            "Italy",
+            "Turkey"
+    };
 
     private ArrayList<Player> players;
     private ArrayList<Country> countries;
--- src/domains/Player.java
+++ src/domains/Player.java
@@ -1,4 +1,16 @@
 package domains;
 
+import java.util.ArrayList;
+
 public class Player {
+
+    private int id;
+    private String name;
+    private Boolean isHost;
+    private Country country;
+    private ArrayList<Unit> units = new ArrayList<Unit>();
+
+    public Player() {
+
+    }
 }
