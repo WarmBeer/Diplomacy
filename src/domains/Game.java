4bd856e06caabe44ccf4e5df8d326bf7d7fe2a0e
WarmBeer <mickvd99@gmail.com>
2019-06-11 11:29:08 +0200

    Domain classes have logic

---
+++ src/domains/Game.java
@@ -0,0 +1,16 @@
+package domains;
+
+import java.util.ArrayList;
+
+public class Game {
+
+    private ArrayList<Player> players;
+    private ArrayList<Country> countries;
+
+    public Game() {
+
+        players = new ArrayList<Player>();
+        countries = new ArrayList<Country>();
+    }
+
+}
