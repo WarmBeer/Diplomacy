33fd065845fb870597075c434502cf82471afc24
sebashh <s_tillema@hotmail.com>
2019-06-12 13:05:35 +0200

    pane van image en chat naar anchorpane gezet en er een menu knopje bijgeplaatst + het menu zelf en wat methodes ervan. Moet denk ik alleen nog even MVC gemaakt worden, daar heb ik wat hulp bij nodig

--- src/controllers/in_GameMenuController.java
+++ src/controllers/in_GameMenuController.java
@@ -1,123 +1,121 @@
 package controllers;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Pagination;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
-import javafx.util.Callback;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.ResourceBundle;
+import models.in_GameMenuModel;
+import views.MainMenu;
 
+public class in_GameMenuController {
 
-public class in_GameMenuController implements Initializable {
+    in_GameMenuModel inGameModel;
 
-    @FXML
-    private VBox rootPane;
-
-    @FXML
-    private VBox MainMenu;
-
-    @FXML
-    private Button MenuButton;
-
-    @FXML
-    private Button OpslaanKnop;
-
-    @FXML
-    private Button LadenKnop;
-
-    @FXML
-    private Button AfsluitenKnop;
-
-    @FXML
-    private Button SpelregelsKnop;
-
-    @FXML
-    private Button OptiesKnop;
-
-    @FXML
-    private void OpenInGameMenu(){
-
-        MainMenu.setVisible(!MainMenu.isVisible());
+    public in_GameMenuController() {
+        inGameModel = new in_GameMenuModel();
     }
 
-    @FXML
-    private void AfsluitenGame() {
-
-        System.exit(0);
+    public void afsluitenController() {
+        inGameModel.afsluitenModel();
     }
 
+//    public void openInGameMenuController() {
+//        inGameModel.openInGameMenuModel();
+//    }
 
 
-    public VBox createPage(int pageIndex) {
-        VBox box = new VBox(5);
-        int page = pageIndex;
-        for (int i = page; i < page; i++) {
-            VBox element = new VBox();
-            Label text = new Label("Search results\nfor ");
-            element.getChildren().addAll(text);
-            box.getChildren().add(element);
-        }
-        return box;
-    }
-
-    @FXML
-    public void CreerPagina() throws IOException {
-        final ArrayList<Image> imagesRules = new ArrayList<>();
-        Pagination p = new Pagination(24);
-
-        p.setPageFactory(new Callback<Integer, Node>() {
-            @Override
-            public Node call(Integer pageIndex) {
-                return createPage(pageIndex);
-            }
-        });
 
 
-        Parent panel;
-        panel = FXMLLoader.load(getClass().getResource("/resources/views/Spelregels.fxml"));
-        Scene scene = new Scene(panel);
-        Stage stage = new Stage();
-        stage.setScene(scene);
-
-        stage.setTitle("Diplomacy v0.1");
-        stage.setMaximized(true);
-        //stage.setResizable(false);
-        stage.show();
-
-        for(int i = 1; i <= 24; i++) {
-            imagesRules.add(new Image("/resources/rules/diplomacy_rulebook-" + i + ".png"));
 
 
-        }
-    }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
-    }
-
-    @FXML
-    private void ShowSpelregels(ActionEvent event) throws IOException {
-        VBox pane = FXMLLoader.load(getClass().getResource("resources/views/Spelregels.fxml"));
-        rootPane.getChildren().setAll(pane);
-        Scene scene = new Scene(rootPane);
 
 
-    }
 
+
+
+
+
+
+
+
+
+
+
+
+
+//    @FXML
+//    private VBox VboxSpelregels;
+//
+//    @FXML
+//    private Pagination paginationrules;
+//
+//    @FXML
+//    private VBox MainMenu;
+//
+//    @FXML
+//    private Button MenuButton;
+//
+//    @FXML
+//    private Button OpslaanKnop;
+//
+//    @FXML
+//    private Button LadenKnop;
+//
+//    @FXML
+//    private Button AfsluitenKnop;
+//
+//    @FXML
+//    private Button SpelregelsKnop;
+//
+//    @FXML
+//    private Button OptiesKnop;
+//
+//    @FXML
+//    private void OpenInGameMenu(){
+//
+//        MainMenu.setVisible(!MainMenu.isVisible());
+//    }
+//
+//    @FXML
+//    private void AfsluitenGame() {
+//
+//        System.exit(0);
+//    }
+//
+//    @FXML
+//    private VBox createPage(int pageIndex) {
+//        final ArrayList<String> imagesRules = new ArrayList<>();
+//        for (int i = 1; i <= 24; i++) {
+//            imagesRules.add("/resources/rules/rulebook-" + i + ".png");
+//        }
+//        VBox box = new VBox();
+//        box.setAlignment(Pos.CENTER);
+//        ImageView iv = new ImageView(String.valueOf(imagesRules.get(pageIndex)));
+//        box.getChildren().addAll(iv);
+//        return box;
+//    }
+//
+////    @FXML
+////    public void CreerPagina() throws IOException {
+////    }
+////        final ArrayList<String> imagesRules = new ArrayList<>();
+////        for (int i = 1; i <= 24; i++) {
+////            imagesRules.add("/resources/rules/rulebook-" + i + ".png");
+////        }
+//
+//
+////        Parent panel;
+////        panel = FXMLLoader.load(getClass().getResource("/resources/views/Spelregels.fxml"));
+////        Scene scene = new Scene(panel);
+////        Stage stage = new Stage();
+////        stage.setScene(scene);
+////
+////        stage.setTitle("Diplomacy v0.1");
+////        stage.setMaximized(true);
+////        //stage.setResizable(false);
+////        stage.show();
+//
+////    }
+//    @Override
+//    public void initialize(URL location, ResourceBundle resources) {
+//        paginationrules.setPageCount(24);
+//        paginationrules.setPageFactory((Integer pageIndex) -> createPage(pageIndex));
+//    }
 }
