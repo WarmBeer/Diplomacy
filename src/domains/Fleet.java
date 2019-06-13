5a21be94710fee93757385727ebeb66c0bf3e48a
WarmBeer <mickvd99@gmail.com>
2019-06-13 11:52:05 +0200

    Army and Fleet colors

--- src/domains/Fleet.java
+++ src/domains/Fleet.java
@@ -3,6 +3,6 @@ package domains;
 public class Fleet extends Unit{
 
     public Fleet(Province province) {
-        super("/Fleet.png", province);
+        super("/Fleet-" + province.getOwner().getName().toString() + ".png", province);
     }
 }
